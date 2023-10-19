7e40a39bded5718a07d56cce56d6bcf121d9df4d
Aryo Bagas Sasongko <106600608+IAmBaghas@users.noreply.github.com>
2023-10-19 08:35:05 +0700

    Game up

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Tembak</title>
+		
+		<style> *{-webkit-tap-highlight-color: rgba(0, 0, 0, 0);}
+				 body {
+					padding: 0;
+					margin: 0;
+				 }
+		</style>
+
+		<script src="shmup.js"></script>
+		
+	</head>
+	<body onload="Main();">
+		<canvas id="Shooter" width="550" height="1000">
+	</body>
+</html>
